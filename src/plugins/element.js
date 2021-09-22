61014099674069ceb9d8937c6480d83ef0d945a1
HoboCY <1830231903@qq.com>
2021-09-22 17:59:55 +0800

    import component

---
+++ src/plugins/element.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import { Button } from 'element-ui'
+import { Container } from 'element-ui'
+import { Main } from 'element-ui'
+import { Aside } from 'element-ui'
+import { Header } from 'element-ui'
+import { Menu } from 'element-ui'
+import { MenuItem } from 'element-ui'
+import { RadioGroup } from 'element-ui'
+import { RadioButton } from 'element-ui'
+import { Row } from 'element-ui'
+import { Col } from 'element-ui'
+import { Card } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
