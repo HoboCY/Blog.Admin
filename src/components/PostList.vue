695678082edf9eba207bc49c909e66e56860c828
HoboCY <1830231903@qq.com>
2021-09-27 18:01:06 +0800

    add PostList Component

---
+++ src/components/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-table :data="posts" stripe style="width: 100%">
+    <el-table-column prop="id" label="日期" width="180"> </el-table-column>
+    <el-table-column prop="title" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="creationTime" label="地址"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  props: ["posts"]
+};
+</script>
