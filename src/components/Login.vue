c624580d735fe4526c73702986cae6439c2e488b
HoboCY <1830231903@qq.com>
2021-09-27 17:59:57 +0800

    add login component and complete login function

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,186 @@
 <template>
-  <div></div>
+  <div class="login">
+    <el-card>
+      <h2>Login</h2>
+      <el-form
+        class="login-form"
+        :model="loginModel"
+        :rules="rules"
+        ref="form"
+        @submit.native.prevent="login"
+      >
+        <el-form-item prop="email">
+          <el-input
+            v-model="loginModel.email"
+            placeholder="Email Address"
+            prefix-icon="fas fa-user"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginModel.password"
+            placeholder="Password"
+            type="password"
+            prefix-icon="fas fa-lock"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            :loading="loading"
+            class="login-button"
+            type="primary"
+            native-type="submit"
+            block
+            >Login</el-button
+          >
+        </el-form-item>
+        <!-- <a class="forgot-password" href="https://oxfordinformatics.com/">Forgot password ?</a> -->
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "login",
+  data() {
+    return {
+      loginModel: {
+        email: "",
+        password: ""
+      },
+      loading: false,
+      rules: {
+        email: [
+          {
+            required: true,
+            message: "请输入邮箱",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            message: "邮箱至少6位",
+            trigger: "blur"
+          },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "Password is required", trigger: "blur" },
+          {
+            min: 6,
+            message: "密码至少6位",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    simulateLogin() {
+      return new Promise(resolve => {
+        setTimeout(resolve, 800);
+      });
+    },
+    async login() {
+      let valid = await this.$refs.form.validate();
+      if (!valid) {
+        return;
+      }
+      this.loading = true;
+      await this.simulateLogin();
+      this.$axios
+        .post("users/login", { Email: this.loginModel.email, Password: this.loginModel.password })
+        .then(res => {
+          window.localStorage.setItem("token", res.data.token);
+          this.$message.success("登陆成功");
+          this.$router.push("/post");
+        })
+        .catch(err => {
+          this.$message.error(err.response.data);
+        });
+      this.loading = false;
+    }
+  }
+};
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: 10%;
+}
 
-<style scoped></style>
+.login-button {
+  width: 100%;
+  margin-top: 40px;
+}
+.login-form {
+  width: 290px;
+}
+.forgot-password {
+  margin-top: 10px;
+}
+</style>
+<style lang="scss">
+$teal: rgb(0, 124, 137);
+.el-button--primary {
+  background: $teal;
+  border-color: $teal;
+
+  &:hover,
+  &.active,
+  &:focus {
+    background: lighten($teal, 7);
+    border-color: lighten($teal, 7);
+  }
+}
+.login .el-input__inner:hover {
+  border-color: $teal;
+}
+.login .el-input__prefix {
+  background: rgb(238, 237, 234);
+  left: 0;
+  height: calc(100% - 2px);
+  left: 1px;
+  top: 1px;
+  border-radius: 3px;
+  .el-input__icon {
+    width: 30px;
+  }
+}
+.login .el-input input {
+  padding-left: 35px;
+}
+.login .el-card {
+  padding-top: 0;
+  padding-bottom: 30px;
+}
+h2 {
+  font-family: "Open Sans";
+  letter-spacing: 1px;
+  font-family: Roboto, sans-serif;
+  padding-bottom: 20px;
+}
+a {
+  color: $teal;
+  text-decoration: none;
+  &:hover,
+  &:active,
+  &:focus {
+    color: lighten($teal, 7);
+  }
+}
+.login .el-card {
+  width: 340px;
+  display: flex;
+  justify-content: center;
+}
+</style>
