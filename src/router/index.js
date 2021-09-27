842b96c625fdda4fba5e7421269ab248c4dd4c3c
HoboCY <1830231903@qq.com>
2021-09-27 18:00:55 +0800

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Category from '../views/Category/Category.vue'
 import Post from '../views/Post/Post.vue'
+import MyPosts from '../views/Post/MyPosts.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -20,12 +22,22 @@ const routes = [
       path: '/post',
       name: 'Post',
       component: Post
+    },
+    {
+      path: '/postlist',
+      name: 'MyPosts',
+      component: MyPosts
     }]
-  }
+  },
+  {
+    path: '/',
+    name: "Login",
+    component: Login
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes, mode: "history"
 })
 
 export default router
