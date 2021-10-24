c78e68f5259eb88856c0cfb0c240b67578e33342
hobocy <1830231903@qq.com>
2021-10-24 21:37:34 +0800

    change function to build routes

--- src/utils/createRoutes.js
+++ src/utils/createRoutes.js
@@ -2,32 +2,21 @@ import { asyncRoutes } from "@/router";
 
 // 将菜单信息转成对应的路由信息 动态添加
 export default function createRoutes(data) {
-  const result = [];
-  const routes = [];
-
-  data.map((item) => {
-    generateRoutes(routes, item);
-  });
-
-  // 最后添加404页面 否则会在登陆成功后跳到404页面
-  //   result.push({ path: "*", redirect: "/404" });
-
+  var routes = buildRoutes(data, asyncRoutes);
   return routes;
 }
 
-var i = 1;
-
-function generateRoutes(routes, item) {
-  var route = asyncRoutes.find((route) => route.name === item.name);
-  var children = [];
-  if (route !== undefined) {
-    item.children.map((child) => {
-      var childRoute = route.children.find((r) => r.name === child.name);
-      if (childRoute !== undefined) {
-        children.push(childRoute);
+function buildRoutes(userRoutes = [], allRoutes = []) {
+  var realRoutes = [];
+  allRoutes.forEach((v, i) => {
+    userRoutes.forEach((item, index) => {
+      if (item.name === v.name) {
+        if (item.children && item.children.length > 0) {
+          v.children = buildRoutes(item.children, v.children);
+        }
+        realRoutes.push(v);
       }
     });
-    route.children = children;
-    routes.push(route);
-  }
+  });
+  return realRoutes;
 }
