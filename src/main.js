68e1b944201dbaafeadcddb4926ffb4dc816af0a
HoboCY <1830231903@qq.com>
2021-10-27 17:28:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ router.beforeEach(async (to, from, next) => {
       return;
     }
     next({ name: "Login" });
-  } else if (to.path === "/") {
+  } else if (to.path === "/" || to.name === "Login") {
     next({ name: "PostList" });
   } else if (to.name === "PostList" && from.name === "Login") {
     if (hasMenus) return next();
